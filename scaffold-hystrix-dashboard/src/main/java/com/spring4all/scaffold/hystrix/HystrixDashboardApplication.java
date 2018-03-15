5cc35592c9efd5add3385f090751bc5b97055247
xiaobaxi <silencon@gmail.com>
2018-03-16 01:34:47 +0800

    update the hystrix

--- scaffold-hystrix-dashboard/src/main/java/com/spring4all/scaffold/hystrix/HystrixDashboardApplication.java
+++ scaffold-hystrix-dashboard/src/main/java/com/spring4all/scaffold/hystrix/HystrixDashboardApplication.java
@@ -2,7 +2,7 @@ package com.spring4all.scaffold.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApplication.class, args);
